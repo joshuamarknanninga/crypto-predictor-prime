b6774c08518c50f6a848b0f2f9603f79677b2283
Joshua Nanninga <jmarknanninga@gmail.com>
2025-01-06 11:32:21 -0600

    changed backend port to 3001

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cryptoRoutes = require('./routes/cryptoRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
